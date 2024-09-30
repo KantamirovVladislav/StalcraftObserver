990bd965ff9bec8a0a2f6a766365bc15240dac42
kanta <kantamirov2016@gmail.com>
2024-09-30 10:23:06 +0300

    add new preview device

--- app/src/main/java/com/example/stalcraftobserver/presentation/onBoarding/components/PagesIndicator.kt
+++ app/src/main/java/com/example/stalcraftobserver/presentation/onBoarding/components/PagesIndicator.kt
@@ -1,15 +1,23 @@
 package com.example.stalcraftobserver.presentation.onBoarding.components
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -21,9 +29,11 @@ fun PagesIndicator(
     val selectedColor = MaterialTheme.colorScheme.primary;
     val unselectedColor = MaterialTheme.colorScheme.secondary;
 
-    Row(modifier = modifier, horizontalArrangement = Arrangement.SpaceBetween) {
+    Row(modifier = modifier, horizontalArrangement = Arrangement.SpaceAround, verticalAlignment = Alignment.Bottom) {
         repeat(pageSize) { page ->
-            Box(modifier = Modifier.size(14.dp).clip(CircleShape)
+            Box(modifier = Modifier.height(height = animateDpAsState(30.dp))//if (page == selectedPage) 40.dp else 30.dp)
+                .width(10.dp)
+                .clip(shape = RoundedCornerShape(size = 5.dp))
                 .background(color = if (page == selectedPage) selectedColor else unselectedColor)) {
 
             }
